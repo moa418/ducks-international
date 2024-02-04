d26f6f5c2ed69eb7598ab21da777bbfbdeee46ac
Benzillaist <benkuchma@gmail.com>
2024-02-04 00:39:33 -0500

    test

--- unity/My project/Assets/Scripts/ItemCollector.cs
+++ unity/My project/Assets/Scripts/ItemCollector.cs
@@ -6,31 +6,30 @@ using UnityEngine.UI;
 public class ItemCollector : MonoBehaviour
 {
     [SerializeField] private GameObject SingleGateUI;
-    [SerializeField] private Sprite xgateSprite;
     [SerializeField] private StageObject stage;
+    [SerializeField] private int qubitID;
 
     private int gateCount = 0;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("X-Gate"))
-        {
-            Destroy(collision.gameObject);
-            gateCount++;
-            AddSingleGateUIElement(xgateSprite);
-        }
-        Gate1Q g = new Gate1Q(collision.gameObject.tag, collision.gameObject.GetComponent<PlayerMovement>().qubitID);
-        stage.gate_list.Add(g);
-        Debug.Log(stage.gate_list[0].ToString());
-
+        gateCount++;
+        AddSingleGateUIElement(collision.gameObject);
+        Gate g = new Gate(collision.gameObject.tag, qubitID);
+        stage.gate_list.Add(new Gate(collision.gameObject.tag, qubitID));
+        Debug.Log(g.ToString());
+        Destroy(collision.gameObject);
     }
 
-    void AddSingleGateUIElement(Sprite gateSprite)
+    void AddSingleGateUIElement(GameObject gate_obj)
     {
+        Sprite gate_sprite = gate_obj.GetComponent<SpriteRenderer>().sprite;
         Vector2 pos = new Vector2(-280 + gateCount*50,-50);
         GameObject uiElement = Instantiate(SingleGateUI, pos, Quaternion.identity);
         uiElement.transform.SetParent(GameObject.Find("Canvas").transform, false);
-        Image gateImage = uiElement.GetComponent<Image>();
-        gateImage.sprite = gateSprite;
-        
+        Image gateImage = uiElement.AddComponent<Image>();
+        gateImage.sprite = gate_sprite;
+        // Image newImage = new Image();
+        // newImage.sprite = gate_sprite;
+        // Image newImage = Instantiate(   , pos, Quaternion.identity);
     }
 }
